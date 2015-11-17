11c99691a08eb808d8bc5dbd07777cdbeb8aac33
n-a-n <loic.truchot35@gmail.com>
2015-11-17 12:30:30 +0700

    favicon & illustrations

--- server.js
+++ server.js
@@ -1,40 +1,33 @@
 var path = require('path');
 var express = require('express');
 var compass = require('node-compass');
-
+var favicon = require('serve-favicon');
 var port = process.env.PORT || 8080;
-
 var app = express();
-
-var path = require('path');
-var express = require('express');
-var compass = require('node-compass');
-
-var app = express();
-
 var dir = {
-	'client': path.join(__dirname, 'client'),
-	'style': path.join(__dirname, 'client', 'css'),
-	'application': path.join(__dirname, 'client', 'views', 'laolol', 'laoletterchallenge'),
-	'public': path.join(__dirname, 'public')
+    'client': path.join(__dirname, 'client'),
+    'style': path.join(__dirname, 'client', 'css'),
+    'application': path.join(__dirname, 'client', 'views', 'index'),
+    'laololPublic': path.join(__dirname, 'public')
 };
 
+app.use(favicon(path.join(dir.laololPublic, 'images', 'favicon', 'favicon.ico')));
 app.use('/client', express.static(dir.client));
 app.use('/style', express.static(dir.style));
 app.use('/application', express.static(dir.application));
-app.use('/public', express.static(dir.public));
-app.get('/', function(req, res){
-	res.sendFile("laoletterchallenge.html", {
-		root: dir.application
-	});  
+app.use('/public', express.static(dir.laololPublic));
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {
+        root: dir.application
+    });
 });
 
 app.use(compass({
-	project:  path.join(__dirname, "client"),
-	sass: 'views/laolol/laoletterchallenge',
-	css: 'css'
+    project: path.join(__dirname, 'client'),
+    sass: 'views/index',
+    css: 'css'
 }));
 
-app.listen(port, function () {
-	console.log("LAOLOL server is running on http://localhost:8080/");
+app.listen(port, function() {
+    console.log('LAOLOL server is running on http://localhost:8080/');
 });
